0f2f7d2c1f924b2c07626d5f048dc84a405f7e15
Ben Morris <the_power@Bens-MacBook-Pro.local>
2018-05-23 21:55:36 -0500

    Established connection to database, implemented login wireframe.

--- Tri-Track/MainActivity.cs
+++ Tri-Track/MainActivity.cs
@@ -1,16 +1,20 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using MySqlConnector;
+using MySql.Data.MySqlClient;
 
 namespace TriTrack
 {
     [Activity(Label = "Tri-Track", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        //int count = 1;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            MySqlConnection connection = new MySqlConnection("server=extremobemotestserver.mysql.database.azure.com;port=3306;database=test;user id=extremobemo@extremobemotestserver;password=Morris98;SslMode=None");
+            connection.Open();
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
@@ -18,9 +22,9 @@ namespace TriTrack
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            Button button = FindViewById<Button>(Resource.Id.button1);
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            //button.Click += delegate { button.Text = $"{count++} clicks!"; };
         }
     }
 }
