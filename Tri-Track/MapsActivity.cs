1854c5136039923fdc6dab93279c36ef570aac55
Ben Morris <extremobemo@gmail.com>
2018-05-28 01:06:38 -0500

    Instantiated Google Maps Fragment in my MapsActivity.

--- Tri-Track/MapsActivity.cs
+++ Tri-Track/MapsActivity.cs
@@ -6,6 +6,7 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Gms.Maps;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -14,13 +15,30 @@ using Android.Widget;
 namespace TriTrack
 {
     [Activity(Label = "MapsActivity")]
-    public class MapsActivity : Activity
+    public class MapsActivity : Activity, IOnMapReadyCallback
     {
+        GoogleMap daMap;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Map);
-            // Create your application here
+            MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.the_fucking_map);
+            mapFragment.GetMapAsync(this);
+            Button switchB = FindViewById<Button>(Resource.Id.switch_button);
+            switchB.Click += delegate {
+
+                daMap.MapType = GoogleMap.MapTypeNormal;
+            };
+        }
+
+      
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            daMap = googleMap;
+            daMap.MapType = GoogleMap.MapTypeHybrid;
+
         }
     }
 }
